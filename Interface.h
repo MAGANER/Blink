7079ae7328796c47233854239c360128ec976032
MAGANER <maganer.krik@mail.ru>
2021-06-17 17:26:58 +0300

    added special output

--- Interface.h
+++ Interface.h
@@ -17,16 +17,26 @@ namespace Blink
 		~Interface(){}
 
 		map<string, function<void()>> commands;
+	private:
 		void run_command(const string& command)
 		{
 			if (commands.find(command) == commands.end())
 			{
-				cout << "command " << command << " doesn't exist!" << endl;
+				cout << "command " <<'`'+ command+'`' << " doesn't exist!" << endl;
 			}
 			else
 			{
 				commands[command]();
+				cout << endl;
 			}
+		}
+	public:
+		void run()
+		{
+			cout << "$";
+			string command;
+			cin >> command;
+			run_command(command);
 		}
 	};
 };
