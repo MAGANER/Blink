0ecda7135cbc3ca55d9c9e3bd55308f1aa57568d
MAGANER <maganer.krik@mail.ru>
2021-06-29 16:11:44 +0300

    added connect command

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -4,9 +4,10 @@ using namespace Blink;
 
 MainMenu::MainMenu()
 {
-	commands["exit"]   = function<void()>([&]() { _exit = true; });
-	commands["create"] = function<void()>([&]() { create();     });
-	commands["enter"]  = function<void()>([&]() { enter();      });
+	commands["exit"]    = function<void()>([&]() { _exit = true; });
+	commands["create"]  = function<void()>([&]() { create();     });
+	commands["enter"]   = function<void()>([&]() { enter();      });
+	commands["connect"] = function<void()>([&]() { connect();   });
 }
 void MainMenu::create()
 {
@@ -58,7 +59,36 @@ bool MainMenu::can_connect(const string& ip,
 
 	char buffer[2];
 	size_t received = 0;
-	socket.receive(buffer, sizeof(buffer), received);
+	auto result = socket.receive(buffer, sizeof(buffer), received);
 	if (string(buffer) == "1") return true;
+	else if (result != TcpSocket::Done) return false;
 	else return false;
+}
+void MainMenu::connect()
+{
+	string ip, room_name, password;
+	int port;
+	cout << "enter ip:";
+	cin >> ip;
+	cout << "enter port:";
+	cin >> port;
+	cout << "enter room name:";
+	cin >> room_name;
+	cout << "enter password:";
+	cin >> password;
+
+	if (can_connect(ip, password, room_name, port))
+	{
+		cout << "connecting to " << room_name << endl;
+		room_ip        = ip;
+		this->password = password;
+		this->port     = to_string(port);
+		room_to_enter  = room_name;
+	}
+	else
+	{
+		cout << "can not connect!";
+	}
+
+
 }
--- MainMenu.h
+++ MainMenu.h
@@ -16,24 +16,30 @@ namespace Blink
 
         bool exit()       { return _exit;          }
         bool enter_room() { return can_enter_room; }
+        bool _connect() { return can_connect_to_room; };
         string get_room_to_enter() { return room_to_enter; }
         string get_password_to_enter() { return password; };
+        string get_room_ip() { return room_ip; };
         string get_port() { return port; }
 
         void finish()
         {
-            can_enter_room = false;
+            can_enter_room      = false;
+            can_connect_to_room = false;
             room_to_enter.clear();
             password.clear();
             port.clear();
+            room_ip.clear();
         }
 
     private:
         bool _exit          = false;
         bool can_enter_room = false;
+        bool can_connect_to_room = false;
         string room_to_enter;
         string password;
         string port;
+        string room_ip;
 
         void create();
         void enter();
