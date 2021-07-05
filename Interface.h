eae59786dda8163d99bee2396f11468f9a7cc7a7
MAGANER <maganer.krik@mail.ru>
2021-07-05 15:05:58 +0300

    added special procedure to process async input

--- Interface.h
+++ Interface.h
@@ -11,6 +11,37 @@ namespace Blink
 {
 	using namespace std;
 
+	/*
+	* I don't like this solution, cos it's pretty C-style procedure,
+	* but, yeah, it's kinda not bad to make it short
+	* and it makes feel like a hacker from 80s.
+	* (BTW, C-hackers from 80s didn't use lambda functions)
+	* ((and C++, too))
+	*/
+	namespace
+	{
+		//overwrapped it to nameless namespace 
+		//to avoid multidefinition error
+		void process_input(AsyncInput& input,
+						   bool& dollar_printed,
+						   function<void(string)> action)
+		{
+			//this async input doesn't block main thread,
+			//so dollar printing will be eternal,
+			//so we we print one again after input data is received
+			if (!dollar_printed)
+			{
+				cout << "$";
+				dollar_printed = true;
+			}
+			input.read();
+			if (input.can_get_result())
+			{
+				action(input.get_result());
+				dollar_printed = false;
+			}
+		}
+	};
 	class Interface
 	{
 	private:
@@ -43,17 +74,8 @@ namespace Blink
 	public:
 		virtual void run()
 		{
-			if (!dollar_printed)
-			{
-				cout << "$";
-				dollar_printed = true;
-			}
-			input.read();
-			if (input.can_get_result())
-			{
-				run_command(input.get_result());
-				dollar_printed = false;
-			}
+			auto action = [&](const string& command) { run_command(command);  };
+			process_input(input, dollar_printed, action);
 		}
 	};
 };
