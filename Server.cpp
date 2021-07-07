093660f89a84282f492dde2cd178d44281e32acd
MAGANER <maganer.krik@mail.ru>
2021-07-08 00:01:23 +0300

    added special socket to send data back

--- Server.cpp
+++ Server.cpp
@@ -11,28 +11,42 @@ Server::~Server()
 bool Server::run(const string& channel_name,
 				 int port)
 {
-	auto callback = [&](const string& data)
-	{
-		json message = json::parse(data);
-		if (connected_ip.empty()) connected_ip = message["ip"];
-		cout << message["name"] << ":" << message["text"]<<endl;
-	};
-	
-	auto listen_to_socket = [&]() {manager.socketListen(channel_name, port, callback); };
-	auto run_server = [&]()
-	{
-		run_socket(listen_to_socket);
-		return true;
-	};
-	auto process = [&](const string& command) {process_commands(command); };
-	auto result = async(std::launch::async, run_server);
-	
-	//process input 
+
+	listener.listen(port);
+	sf::TcpSocket socket;
+	listener.accept(socket);
+
+
 	while (true)
 	{
+		Packet pack;
+		string data;
+		socket.receive(pack);
+		/*if (!is_first_message_received)
+		{
+			string connection_data;
+			pack >> connection_data;
+			json data = json::parse(connection_data);
+			
+			connected_ip = fp::slice(_s(data["ip"]), 1, _s((data["ip"])).size());
+			connected_port = data["port"];
+
+			is_first_message_received = true;
+		}
+		else
+		{*/
+		pack >> data;
+		cout << "got:" << data << endl;
+		//}
+
+		data.clear();
+		cout << "enter:";
+		cin >> data;
+		pack.clear();
+		pack << data;
+		socket.send(pack);
 	}
-	result.get(); //don't need it
-	return false;
+	return true;
 }
 void Server::process_commands(const string& command)
 {
--- Server.h
+++ Server.h
@@ -1,32 +1,38 @@
 #ifndef SERVER_H
 #define SERVER_H
-#include"SocketRunner.h"
-#include"Interface.h" // for process_input procedure
+#define _s(str) string(str)
+
+#include"SFML/Network.hpp"
 #include<string>
 #include<iostream>
 #include<future>
 #include"json.hpp"
+#include"sql/Functools.hpp"
 
 namespace Blink
 {
 	using namespace std;
-	using namespace masesk;
 	using namespace nlohmann;
+	using namespace sf;
+	namespace fp = Functools;
 
 	class Server
 	{
 	private:
-		EasySocket manager;
 		string connected_ip;
+		int connected_port;
+
+		TcpListener listener;
+		bool is_first_message_received = false;
 	public:
 		Server();
 		~Server();
 
 		bool run(const string& channel_name,
 				 int port);
+		string get_connected_ip() { return connected_ip; }
+		int get_connected_port() { return connected_port; }
 	private:
-		AsyncInput input;
-		bool dollar_printed = false;
 		void process_commands(const string& command);
 	};
 };
