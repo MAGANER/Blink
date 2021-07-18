dcca87993ab42a44e96ca76996f0298b697b1a4d
MAGANER <maganer.krik@mail.ru>
2021-07-18 16:05:32 +0300

    contains special functions to process json files

---
+++ MessageCreator.h
@@ -0,0 +1,40 @@
+#ifndef MESSAGE_CREATOR_H
+#define MESSAGE_CREATOR_H
+#include"json.hpp"
+#include<string>
+#include<iostream>
+namespace Blink
+{
+namespace
+{
+	using namespace std;
+	using namespace nlohmann;
+	enum class MessageType
+	{
+		Text,
+		ComeInRequest
+	};
+
+	string convert_message_to_json(const string& data,
+								   const string& name,
+								   MessageType type)
+	{
+		json jdata;
+		jdata["type"] = (int)type;
+		jdata["data"] = data;
+		jdata["name"] = name;
+		string result = jdata.dump();
+		return result;
+	}
+	bool can_come_in(const string& message,
+					 const string& password,
+					 const string& name)
+	{
+		json jdata = json::parse(message);
+		return jdata["data"] == password &&
+			   jdata["name"] == name;
+	}
+	
+};
+};
+#endif //MESSAGE_CREATOR_H
