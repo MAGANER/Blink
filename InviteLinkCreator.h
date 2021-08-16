613910d6410d552cc4ec902eae806ee38f9ff855
MAGANER <maganer.krik@mail.ru>
2021-08-16 19:32:30 +0300

    improved macro

--- InviteLinkCreator.h
+++ InviteLinkCreator.h
@@ -4,7 +4,9 @@
 
 #pragma once
 #ifdef _MSC_VER
+#ifndef _CRT_SECURE_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS
+#endif
 #endif
 #include<chrono>
 #include<string>
--- MainMenu.h
+++ MainMenu.h
@@ -3,7 +3,9 @@
 */
 #pragma once
 #ifdef _MSC_VER
+#ifndef _CRT_SECURE_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS
+#endif
 #endif
 #include"SFML/Network.hpp"
 #include "Interface.h"
