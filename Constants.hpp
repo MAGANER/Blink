6cc91d89370c00e5299cdfcc3dc000e08026baa1
MAGANER <maganer.krik@mail.ru>
2021-10-04 15:43:21 +0300

    added some static values like max length of name or password, also min vals

---
+++ Constants.hpp
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+namespace Blink
+{
+	static const int MAX_PASSWORD_LEN = 256;
+	static const int MIN_PASSWORD_LEN = 8;
+
+	static const int MAX_NAME_LEN = 32;
+	static const int MIN_NAME_LEN = 2;
+};
+#endif 
